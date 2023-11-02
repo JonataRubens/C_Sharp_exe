dd00b639100edb96940335bbbb5e8c0393b234c5
JONATA <105253719+JonataRubens@users.noreply.github.com>
2023-11-02 15:38:40 -0300

    up

--- Exe/Soma dos Números Pares/Soma dos Números Pares/Program.cs
+++ Exe/Soma dos Números Pares/Soma dos Números Pares/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número inteiro positivo N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        if(N == 0)
+        {
+            Console.WriteLine("numero invalido");
+            return;
+        }
+
+        int soma = 0;
+
+        for (int i = 1; i<= N; i++)
+        {
+            if (i%2 == 0)
+            {
+                soma +=i;
+            }
+        }
+        Console.WriteLine($"A soma dos números pares de 1 a \"{N}\" é: \"{soma}");
+    }
+}
