3f256c29eb388abd1627088d2be699545135e746
JONATA <105253719+JonataRubens@users.noreply.github.com>
2023-11-21 12:28:05 -0300

    up

--- Exe/Impar_par/verificar impar_par/Program.cs
+++ Exe/Impar_par/verificar impar_par/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("num inteiro");
+        if(int.TryParse(Console.ReadLine(), out int userInput))
+        {
+            int result = CalculateSquare(userInput);
+            Console.WriteLine($"O quadrado do número {userInput} é {result}.");
+
+            if (result % 2 == 0)
+            {
+                Console.WriteLine("O resultado é um número par.");
+            }
+            else
+            {
+                Console.WriteLine("O resultado é um número ímpar.");
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida. Por favor, digite um número inteiro.");
+        }
+    }   
+    static int CalculateSquare(int number)
+    {
+        return number * number;
+    }
+}
