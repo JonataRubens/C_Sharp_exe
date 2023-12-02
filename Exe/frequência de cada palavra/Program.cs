60c64350f2f19d3ef72309c0bde213f30ca4403b
JONATA <105253719+JonataRubens@users.noreply.github.com>
2023-12-01 23:08:43 -0300

    a ser feito ainda

---
+++ Exe/frequência de cada palavra/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite uyma frase");
+        string input = Console.ReadLine();
+
+        Dictionary<string, int> frequenciaPalavras = ContarFrequenciaPalavras(input);
+    }
+
+    static Dictionary<string, int> ContarFrequenciaPalavras(string input)
+    {
+
+    }
+}
