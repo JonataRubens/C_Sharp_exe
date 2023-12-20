f1f76e7927debd750482f48bba73d2b8022257c0
JONATA <105253719+JonataRubens@users.noreply.github.com>
2023-12-20 13:34:59 -0300

    up

---
+++ Exe/inverter palavras/Program.cs
@@ -0,0 +1,27 @@
+﻿class Name
+{
+    static void Main()
+    {
+        Console.WriteLine("escreva uma frase");
+        string input = Console.ReadLine();
+
+        string resultado = InverterPalavras(input);
+
+        Console.WriteLine("\nPalavra invertida");
+        Console.WriteLine(resultado);
+    }
+    static string InverterPalavras(string input)
+    {
+        string[] palavra = input.Split(' ');
+
+        for(int i = 0; i < palavra.Length; i++)
+        {
+            char[] letras = palavra[i].ToCharArray();
+            Array.Reverse(letras);
+            palavra[i] = new string(letras);
+        }
+
+        return string.Join(" ", palavra);
+    }
+}
+    
