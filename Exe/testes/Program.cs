760e16b517d29006c8d29850f7752891617bf1f1
JONATA <105253719+JonataRubens@users.noreply.github.com>
2024-01-08 15:47:13 -0300

    exe dictionary

---
+++ Exe/testes/Program.cs
@@ -0,0 +1,43 @@
+﻿class Dicts
+{
+    static void Main(string[] args)
+    {
+        Dictionary<int, string> info = new Dictionary<int, string>();
+
+        var dict2 = new Dictionary<int, int>()
+        {
+            {1,100},
+            {2,200},
+            {4,400},
+        };
+
+        info.Add(1, "Info cba1");
+        info.Add(2, "info abc 2");
+
+        var res = dict2.TryAdd(5, 333);
+        Console.WriteLine("elemento foi incluido? " + res);
+        int n = 2;
+        if (!dict2.ContainsKey(n))
+        {
+            dict2.Add(7, 700);
+        }
+        else
+        {
+            Console.WriteLine("key" + n + "ja foi incluida");
+        }
+
+        if(dict2.TryGetValue(n, out int key)) 
+        {
+            Console.WriteLine("Valor para chave " + n + "=" + key);
+        }
+
+        foreach(var item in dict2)
+        {
+            Console.WriteLine($"{item.Key} {item.Value}");
+        }
+        foreach (var item in info)
+        {
+            Console.WriteLine($"{item.Key} {item.Value}");
+        }
+    }
+}
